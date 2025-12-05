3765195024598dc2bb18f75e7fae1f655754ffa7
Kalindu-Karunarathna <kalindukarunarathne626@gmail.com>
2025-12-05 18:21:07 +0530

    create snake body

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 
 
-
-
+for i in range(0,3):
+    snake = Turtle()
+    snake.color("white")
+    snake.shape("square")
+    snake.shapesize(1,1)
+    snake.goto(-20*i,0)
 
 
 
